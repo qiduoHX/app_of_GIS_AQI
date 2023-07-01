27781f7741afd9faf3d7420929802c5774cf1763
Huang Xin <136901592+qiduoHX@users.noreply.github.com>
2023-07-01 11:11:00 +0800

    4_Cities_with_Low_AQI_in_China.py

--- pages/4_Cities_with_Low_AQI_in_China.py
+++ pages/4_Cities_with_Low_AQI_in_China.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 with st.spinner("正在加载数据，请稍候..."):
     import time
-    time.sleep(15)
+    time.sleep(3)
 
 # 获取当前北京时间的小时数
 current_time = datetime.now().strftime("%Y年%m月%d日%H") + "时"
